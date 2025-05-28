866e2f20d4b9cc8558f58ad651370e47d0d933f1
exploring-solver <amansharma12607@gmail.com>
2025-05-29 01:37:40 +0530

    first commit

---
+++ MiniKV/README.md
@@ -0,0 +1,5 @@
+### MiniKV – A Lightweight Key-Value Store in C++
+
+- C++17 or later
+- CMake (build system)
+- GoogleTest (for unit testing)
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout<<"Hello, World!"<<endl;
+    cout<<"This is a C++ program using the C++17 standard."<<endl;
+}
